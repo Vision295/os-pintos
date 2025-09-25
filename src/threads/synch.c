cb2a711f7e59bfc6b5c402e36509e8e39ac139fa
Vision295 <theophilewachtel@gmail.com>
2025-09-25 03:23:30 +0000

    few changes

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -69,7 +69,7 @@ sema_down (struct semaphore *sema)
   while (sema->value == 0) 
     {
       // PRIORITY SCHEDULER
-      list_insert_ordered (&sema->waiters, &thread_current ()->elem, list_less_func *less_priority, NULL);
+      list_insert_ordered (&sema->waiters, &thread_current ()->elem, *less_priority, NULL);
       //list_push_back (&sema->waiters, &thread_current ()->elem);
       thread_block ();
     }
@@ -115,11 +115,12 @@ sema_up (struct semaphore *sema)
   ASSERT (sema != NULL);
 
   old_level = intr_disable ();
-  if (!list_empty (&sema->waiters)) 
+  if (!list_empty (&sema->waiters)) {
     thread_unblock (list_entry (list_pop_front (&sema->waiters),
                                 struct thread, elem));
     // PRIORITY SCHEDULER
     check_preemtion();
+  }
   sema->value++;
   intr_set_level (old_level);
 }
